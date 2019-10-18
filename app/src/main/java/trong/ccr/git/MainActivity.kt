3c80618de6846e96c49160753fe9c9aace53d072
concaro <miss.meomeo@gmail.com>
2019-10-18 11:37:50 +0700

    Initialize project

---
+++ app/src/main/java/trong/ccr/git/MainActivity.kt
@@ -0,0 +1,19 @@
+package trong.ccr.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import trong.ccr.git.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
